3334d9781a6df10659d1fede9753ee208e0e0a7d
Vaidik Rungta <rungtavaidik@gmail.com>
2023-05-09 18:01:47 +0530

    + Integrate Electron

---
+++ app/Main.js
@@ -0,0 +1,32 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+ // mainWindow.setMenuBarVisibility(false)
+ 
+  mainWindow.loadURL('http://localhost:3000');
+}
+
+app.on('ready', async () => {
+  createWindow();
+});
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', function () {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
