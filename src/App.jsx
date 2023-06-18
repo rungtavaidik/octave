5bf090b48071b8decb7c69cee3511cf6337b1be3
Vaidik Rungta <rungtavaidik@gmail.com>
2023-06-18 19:06:28 +0530

    Finish SignUp Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
 import './tailwind.css';
 
-import Login from './components/Login';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import Login from './components/Login';
 import SignUp from './components/SignUp';
+import Dashboard from './pages/Dashboard';
 
 function App() {
   return (
     <div className="App" style={{ fontFamily: 'Montserrat' }}>
       <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Login/>}/>
-        <Route path='/signup' element={<SignUp/>}/>
-      </Routes>
+        <Routes>
+          <Route path='/' element={<Login />} />
+          <Route path='/signup' element={<SignUp />} />
+          <Route path='/dash' element={<Dashboard />} />
+        </Routes>
       </BrowserRouter>
     </div>
   );
